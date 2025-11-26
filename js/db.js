ed4fc5cec6e1e5158ad466e70587e28c99ca2cbd
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-11-25 23:50:48 -0400

    style: Add styling and responsiveness for expense list

--- js/db.js
+++ js/db.js
@@ -73,11 +73,19 @@ const mostrarDespezas = async () => {
 
     if (despezas) {
         document.getElementById('saida').innerHTML = await despezas.map(el => `
-        <tr>
-            <td>${el.categoria}(${el.nota})</td>
-            <td>${el.data}</td>
-            <td>${el.custo}</td>
-        </tr>
+                <div class="item-movimentacao">
+                    <div class="categoria-e-nota">
+                        <span class="item-movimentacao-categoria">${el.categoria}</span>
+                        <span class="item-movimentacao-nota">(${el.nota})</span>
+                    </div>
+                    <span class="item-movimentacao-data">${el.data}</span>
+                    <span class="item-movimentacao-valor">${el.custo}</span>
+                    <div class="item-movimentacao-lixeira"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 11.6666H8.33333V33.3333C8.33333 34.2173 8.68452 35.0652 9.30964 35.6903C9.93476 36.3154 10.7826 36.6666 11.6667 36.6666H28.3333C29.2174 36.6666 30.0652 36.3154 30.6904 35.6903C31.3155 35.0652 31.6667 34.2173 31.6667 33.3333V11.6666H10ZM16.6667 31.6666H13.3333V16.6666H16.6667V31.6666ZM26.6667 31.6666H23.3333V16.6666H26.6667V31.6666ZM27.6967 6.66659L25 3.33325H15L12.3033 6.66659H5V9.99992H35V6.66659H27.6967Z" fill="#333333"/>
+</svg>
+</div>
+                </div>
+                <div class="linha-divisoria"></div>
         `).join('');
     }
 };
