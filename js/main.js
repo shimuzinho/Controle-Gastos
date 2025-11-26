ca5cd75fa12b03f01679d0a0d700b3aa29da26be
Pedro Rossini <pedro.moraes2@estudante.ifms.edu.br>
2025-11-25 21:20:02 -0400

    feat: implement camera access and photo capture

--- js/main.js
+++ js/main.js
@@ -6,6 +6,35 @@ if ('serviceWorker' in navigator) {
             console.log('Service worker registrada!', reg);
         } catch (err) {
             console.log('Service worker não registrada', err);
-        }   
+        }
     });
 }
+
+let contrains = { video: { facingMode: 'user' }, audio: false };
+
+const cameraView = document.getElementById('cameraView');
+const cameraOutput = document.getElementById('cameraOutput');
+const cameraSensor = document.getElementById('cameraSensor');
+const cameraTrigger = document.getElementById('cameraTrigger');
+
+const cameraStart = () => {
+    navigator.mediaDevices
+    .getUserMedia(contrains)
+    .then(stream => {
+        stream.getTracks[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(err => {
+        console.error('Ocorreu um erro: ' + err);
+    });
+};
+
+cameraTrigger.addEventListener('click', () => {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.heigth = cameraView.videoHeigth;
+    cameraSensor.getContext('2d').drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL('images/webp');
+    cameraOutput.classList.add('taken');
+});
+
+window.addEventListener('load', cameraStart, false);
